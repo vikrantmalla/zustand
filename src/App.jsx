44bbd25bd71753f32aa6195db46537e3d491993a
Vikrant Malla <vikrantmalla999@gmail.com>
2023-07-11 20:49:50 +0545

    feat: add a counter in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { create } from 'zustand';
+import useAppStore from '../src/store/appStore';
 import './App.css'
 
-const useStore = create((set) => ({
-  helloCount: 0,
-  increaseCount: () => set((state) => ({ helloCount: state.helloCount + 1 })),
-}));
-
 function App() {
-  const helloCount = useStore((state) => state.helloCount);
-  const increaseCount = useStore((state) => state.increaseCount);
+  const {
+    counter,
+    increaseCounter,
+    decreaseCounter,
+    increaseBy,
+    decreaseBy,
+    reset,
+  } = useAppStore();
   return (
     <>
-     <div>
-      <h1>Hello Zustand {helloCount}</h1>
-      <button onClick={increaseCount}>Increase No</button>
-     </div>
+      <div>
+        <h1>{counter}</h1>
+        <div style={{ display: "flex", flexDirection: "row", gap: "2rem" }}>
+          <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+            <button onClick={increaseCounter}>+</button>
+            <button onClick={decreaseCounter}>-</button>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+            <button onClick={() => increaseBy(10)}>Increase by 10</button>
+            <button onClick={() => decreaseBy(10)}>Decrease by 10</button>
+          </div>
+          <button onClick={reset}>Reset</button>
+        </div>
+      </div>
     </>
   )
 }
